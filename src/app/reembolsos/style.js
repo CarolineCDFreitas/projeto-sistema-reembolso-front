bc3a506211bcd050a20ecde2ae3536a565a090e4
CarolineCaetano <caroline_bdl@hotmail.com>
2025-03-27 09:01:33 -0300

    style: declarar min-height e remover gap do main da página Reembolsos

--- src/app/reembolsos/style.js
+++ src/app/reembolsos/style.js
@@ -1,9 +1,9 @@
 import styled from "@emotion/styled";
 
 export const MainContentWrapper = styled.main`
-/* border: 2px solid darkorchid; */
-display: flex;
-flex-direction: column;
-gap: 2rem;
-margin: 1.5625rem 1.5625rem 1.5625rem 6.5rem;
-`
+  /* border: 2px solid darkorchid; */
+  display: flex;
+  flex-direction: column;
+  margin: 1.5625rem 1.5625rem 1.5625rem 6.5rem;
+  min-height: 92vh;
+`;
