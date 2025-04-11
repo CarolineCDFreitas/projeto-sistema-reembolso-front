8516bd8b11ea428202dd22d50dd18e4042419d82
CarolineCaetano <caroline_bdl@hotmail.com>
2025-04-11 00:02:59 -0300

    feat: componente MessageError para exibir mensagens de erro nos formulários

---
+++ src/app/reembolsos/components/MessageError/MessageError.jsx
@@ -0,0 +1,7 @@
+import { Message } from "../MessageError/MessageErrorStyled";
+
+function MessageError({ children }) {
+  return <Message>{children}</Message>;
+}
+
+export default MessageError;
---
+++ src/app/reembolsos/components/MessageError/MessageErrorStyled.jsx
@@ -0,0 +1,15 @@
+import styled from "@emotion/styled";
+
+export const Message = styled.span`
+  position: absolute;
+  background-color: #ffe4ea;
+  border: solid #a60b2f;
+  border-width: 0 0 0 6px;
+  color: #a60b2f;
+  border-radius: 0.375rem;
+  font-family: 0.625rem;
+  width: 12.5rem;
+  padding: 0.25rem;
+  top: 94%;
+  left: 0;
+`;
