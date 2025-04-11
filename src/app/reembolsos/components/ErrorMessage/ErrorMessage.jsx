7684b43a6ebc76cebd88488f7ac2a3346a59edb0
CarolineCaetano <caroline_bdl@hotmail.com>
2025-04-11 11:37:16 -0300

    chore: corrige nome do componente de MessageError para ErrorMessage

---
+++ src/app/reembolsos/components/ErrorMessage/ErrorMessage.jsx
@@ -0,0 +1,7 @@
+import { Message } from "./ErrorMessageStyled";
+
+function ErrorMessage({ children }) {
+  return <Message>{children}</Message>;
+}
+
+export default ErrorMessage;
--- src/app/reembolsos/components/MessageError/MessageError.jsx
+++
@@ -1,7 +0,0 @@
-import { Message } from "../MessageError/MessageErrorStyled";
-
-function MessageError({ children }) {
-  return <Message>{children}</Message>;
-}
-
-export default MessageError;
