867e9dd01226149b0867932c4044ce7a1ea24b10
CarolineCaetano <caroline_bdl@hotmail.com>
2025-05-10 19:58:22 -0300

    fix: atualiza baseURL da API para o servidor de produção

--- src/services/api/api.js
+++ src/services/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5002",
+  baseURL: "https://projeto-sistema-reembolso-back.onrender.com",
 });
 
 export default api;
