8b9f7a9c55d8a3ffffd4864cbdbba3a74ab4f775
CarolineCaetano <caroline_bdl@hotmail.com>
2025-05-12 18:15:15 -0300

    feat: adiciona interceptor no axios para incluir token de autenticação nas requisições

--- src/services/api/api.js
+++ src/services/api/api.js
@@ -1,7 +1,18 @@
 import axios from "axios";
+import { retrieveToken } from "../auth/authServices";
 
 const api = axios.create({
   baseURL: "http://localhost:5002",
+});
+
+api.interceptors.request.use(function (config) {
+  const token = retrieveToken();
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
 });
 
 export default api;
